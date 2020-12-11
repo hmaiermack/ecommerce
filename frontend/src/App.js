1240348199ffaa89f5e5f3cb76a92336cd6aa49c
Mack Maier <hmaiermack@gmail.com>
2020-12-10 16:51:48 -0800

    begin basic layout and implement bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+        <main className="py-3">
+          <Container>
+            <h1>h1 container</h1>
+          </Container>
+        </main>
+      <Footer />
+    </>
   );
 }
 
