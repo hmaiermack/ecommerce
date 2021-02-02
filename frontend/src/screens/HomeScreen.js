25c2cab16c02bd403b1fe051bd7458bf682f5518
Mack Maier <hmaiermack@gmail.com>
2021-02-01 17:02:12 -0800

    added spinner component to homepage during GET request, added error component if fail GET

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product.js'
+import Loader from '../components/Loader.js'
+import Message from '../components/Message.js'
 import { listProducts } from '../actions/productActions'
-import { productListReducer } from '../reducers/productReducers.js'
 
 const HomeScreen = () => {
     const dispatch = useDispatch()
@@ -18,8 +19,9 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? ( <h2>loading</h2> ) : error ? ( <h3>{error}</h3> ) :
-            <Row>
+            {loading ? ( <Loader /> ) 
+            : error ? ( <Message variant='danger'>{error}</Message> ) 
+            :<Row>
                 {products.map(product => (
                         <Col sm={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
