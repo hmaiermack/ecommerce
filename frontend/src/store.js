d6415ee585c2774cb8ce4285bfed623455fdd38a
Mack Maier <hmaiermack@gmail.com>
2021-04-29 20:52:27 -0700

    user auth and front end profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,28 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers'
+import { useDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers.js'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: useDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse
 (localStorage.getItem('cartItems')) : []
 
+const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse
+(localStorage.getItem('userInfo')) : null
+
+
 const initialState = {
-    cart: { cartItems: cartItemsFromLocalStorage }
+    cart: { cartItems: cartItemsFromLocalStorage },
+    userLogin: { userInfo: userInfoFromLocalStorage}
 }
 
 const middleware = [thunk]
