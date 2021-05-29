1215e12235e541860adba0c86c7ca3195177f72e
Mack Maier <hmaiermack@gmail.com>
2021-05-29 11:06:30 -0700

    admin product list front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import EditUserScreen from './screens/EditUserScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
             <Route path ='/cart/:id?' component={CartScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
             <Route path='/admin/user/:id/edit' component={EditUserScreen} />
+            <Route path ='/admin/productlist' component={ProductListScreen} />
             <Route path ='/' component={HomeScreen} exact />
           </Container>
         </main>
